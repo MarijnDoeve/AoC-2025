86cb3f3df86987f27589cfba136d6035fde40d6f
Marijn Doeve <marijn@doeve.me>
2025-12-04 09:36:21 +0100

    Combine part 1 and 2

--- day4/main.go
+++ day4/main.go
@@ -31,8 +31,38 @@ func main() {
 	maxX := len(grid[0]) - 1
 	maxY := len(grid) - 1
 
+	// Part 1
 	total := 0
+	for y, row := range grid {
+		for x, value := range row {
+			if !value {
+				continue
+			}
 
+			adjecentRolls := 0
+
+			for i := max(0, y-1); i <= min(maxY, y+1); i++ {
+				for j := max(0, x-1); j <= min(maxX, x+1); j++ {
+					if i == y && j == x {
+						continue
+					}
+
+					if grid[i][j] {
+						adjecentRolls++
+					}
+				}
+			}
+
+			if adjecentRolls < 4 {
+				total++
+			}
+		}
+	}
+
+	fmt.Println("Part 1:", total)
+
+	// Part 2
+	total = 0
 	for {
 		removed := false
 		for y, row := range grid {
